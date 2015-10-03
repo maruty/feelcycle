a013ad2a2be5543485e7458cfec0f6db9665e4a9
maruty <01008973@CA0315.local>
2015-10-03 13:43:55 +0900

    mypage新規作成

--- src/main/java/com/marublo/feelcycle/action/ApiRegistAction.java
+++ src/main/java/com/marublo/feelcycle/action/ApiRegistAction.java
@@ -152,6 +152,10 @@ public class ApiRegistAction {
     		resultMessage = "false";
     	}
     	
+    	if(loginId.equals("") || loginPass.equals("")){
+    		resultMessage = "false";
+    	}
+    	
     	return "check.jsp";
     }
     
